Correct sign of jacobian in flow hmc effective action

flows/flow_hmc.py
@@ -30,12 +30,10 @@ def load_flow_action_gradient(phis, model):
     
     flowed_phis, S, logJ = apply_flow_to_fields(phis, model)
     
-    external_grad_S = torch.ones(S.shape)
-    external_grad_logJ = torch.ones(logJ.shape)
+    Seff = S - logJ
+    external_grad_Seff = torch.ones(Seff.shape)
     
-    logJ.backward(gradient=external_grad_logJ, retain_graph=True)
-    S.backward(gradient=external_grad_S)
-
+    Seff.backward(gradient=external_grad_Seff)
 
 def flow_hmc(phi, model, *, tau, n_steps, reversibility = False):
     """
@@ -91,7 +89,7 @@ def flow_Hamiltonian(mom, phi_tilde, model):
     Computes the Hamiltonian of `flow_hmc` function.
     """
     phi_aux, S, logJ = apply_flow_to_fields(phi_tilde, model)
-    H = 0.5 * torch.sum(mom**2) + S.detach() + logJ.detach()
+    H = 0.5 * torch.sum(mom**2) + S.detach() - logJ.detach()
     return H.item()
 
 
@@ -121,3 +119,50 @@ def check_force(phi, model, epsilon):
     
     return ((S_i+log_i-S_eps-log_eps)/epsilon)
 
+
+
+def low_flow_hmc(phi, model, *, tau, n_steps, reversibility = False):
+    """
+    Applies HMC evolution on `phi` with a coordinate transformation given by
+    `model`. Input must be a torch tensor of the form `(1,1,L,L)`, with
+    `L` the lattice size. This function overwrites `phi`, if configuration is
+    accepted.
+
+    `phi` is supposed to be from the target probability distribution. The
+    inverse of the layers in `model` map `phi` to a configuration
+    `phi_tilde` from a probability distribution easier to sample from.
+    """
+
+    # Get phi_tilde = f^{-1}(phi), where f is the trained NN
+    phi_tilde = torch.clone(phi).detach()
+    phi_tilde.requires_grad = True
+    phi_tilde.grad = torch.zeros(phi_tilde.shape) # initialize gradient
+    
+    # Initialize momenta
+    mom = torch.randn(phi.shape)
+    
+    # Initial Hamiltonian
+    H_0 = flow_Hamiltonian(mom, phi_tilde, model)
+    
+    # Leapfrog integrator
+    flow_leapfrog(mom, phi_tilde, model, tau = tau, n_steps = n_steps)
+
+    # Final Hamiltonian
+    dH = flow_Hamiltonian(mom, phi_tilde, model) - H_0
+    
+    if reversibility:
+        phi_tilde_cp = torch.clone(phi_tilde).detach()
+        phi_tilde_cp.requires_grad = True
+        phi_tilde_cp.grad = torch.zeros(phi_tilde_cp.shape)
+
+        flow_leapfrog(-mom, phi_tilde_cp, model, tau = tau, n_steps = n_steps)
+        phi_tilde_cp.requires_grad = False
+
+        print("preReversibility: \sum (\Delta \phi)^2 = ", torch.sum((phi_tilde_cp - phi)**2))
+
+    print("dH = ", dH)
+    if dH > 0:
+        if np.random.rand() >= np.exp(-dH):
+            return False
+    phi[:] = phi_tilde.detach() # element-wise assignment
+    return True