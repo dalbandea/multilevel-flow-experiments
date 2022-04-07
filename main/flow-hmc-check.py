79eafacb0fef7bea59500f031d08541d479f0b1b
dalbandea <albandeadavid@gmail.com>
2022-04-07 15:20:43 +0100

    Update flow-hmc-check.py to load models from checkpoint

--- main/flow-hmc-check.py
+++ main/flow-hmc-check.py
@@ -38,62 +38,14 @@ IterableDataset: TypeAlias = torch.utils.data.IterableDataset
 logging.getLogger().setLevel("WARNING")
 
 
-##############
-#   LAYERS   #
-##############
-
-ADDITIVE_BLOCK = {
-    "transform": transforms.PointwiseAdditiveTransform,
-    "transform_spec": {},
-    "net_spec": {
-        "hidden_shape": [4, 4],
-        "activation": torch.nn.Tanh(),
-        "final_activation": torch.nn.Identity(),
-        "use_bias": False,
-    },
-}
-AFFINE_BLOCK = {
-    "transform": transforms.PointwiseAffineTransform,
-    "transform_spec": {},
-    "net_spec": {
-        "hidden_shape": [4, 4, 4, 4],
-        "activation": torch.nn.Tanh(),
-        "final_activation": torch.nn.Tanh(),
-        "use_bias": False,
-    },
-}
-SPLINE_BLOCK = {
-    "transform": transforms.PointwiseRationalQuadraticSplineTransform,
-    "transform_spec": {"n_segments": 8, "interval": (-4, 4)},
-    "net_spec": {
-        "hidden_shape": [4],
-        "activation": torch.nn.Tanh(),
-        "final_activation": torch.nn.Identity(),
-        "use_bias": True,
-    },
-}
-
-##############
-# MODEL SPEC #
-##############
-
-MODEL_SPEC = [
-    AFFINE_BLOCK,
-    AFFINE_BLOCK,
-    "rescaling",
-]
-
-
 #############
 #  PARSING  #
 #############
 
-def parse_info(info, c):
-    return info.split(c)[1].split("_")[0]
-
 parser = argparse.ArgumentParser()
 # python3 main/flow_hmc-check.py -n NTRAJ -t TAU -ns NSTEPS
 # python3 main/flow-hmc-check.py -n 1 -t 1.0 -ns 1 -w results/trained_networks/testdir/ --model=models/L8_b0.7_l0.5_model.pth
+parser.add_argument("-L", "--lsize", help="Lattice size", type=int, required=True)
 parser.add_argument("-n", "--ntraj", help="Number of trajectories", type=int, required=True)
 parser.add_argument("-t", "--tau", help="HMC trajectory length", default=1.0, type=float)
 parser.add_argument("-ns", "--nsteps", help="Number of integration steps", default=10, type=int)
@@ -136,16 +88,22 @@ os.system("echo Estate of repository: >> {}".format(logdir+"branchinfo.txt"))
 os.system("git show-ref >> {}".format(logdir+"branchinfo.txt"))
 
 
-###############
-#  LOAD MODEL #
-###############
 
-model_name = args.model
+###############################
+#  LOAD MODEL AND HMC PARAMS  #
+###############################
+
+model_path = args.model
+
+# Load model
+model = MultilevelFlow.load_from_checkpoint(model_path)
+model.eval()
 
 # Target theory
-LATTICE_LENGTH = int(parse_info(model_name, "L"))
-BETA = float(parse_info(model_name, "_b"))
-LAM = float(parse_info(model_name, "_l"))
+LATTICE_LENGTH = args.lsize
+BETA = model.hparams.beta
+LAM = model.hparams.lam
+wdir_prefix = "L"+str(LATTICE_LENGTH)+"_b"+str(BETA)+"_l"+str(LAM)
 
 # HMC params
 tau = args.tau
@@ -153,36 +111,27 @@ n_steps = args.nsteps
 ntraj = args.ntraj
 nsave = args.save
 
-# Load model
-model = MultilevelFlow(
-    beta=BETA,
-    lam=LAM,
-    model_spec=MODEL_SPEC,
-)
-
-model.load_state_dict(torch.load(args.model))
-
+# Initial sample from normal distribution
 dist = torch.distributions.Normal(
     loc=torch.zeros((LATTICE_LENGTH, LATTICE_LENGTH)),
     scale=torch.ones((LATTICE_LENGTH, LATTICE_LENGTH)),
 )
-
 val_dataloader = Prior(dist, sample_shape=[1, 1])
 phi = val_dataloader.sample()
 
-wdir_prefix = "L"+str(LATTICE_LENGTH)+"_b"+str(BETA)+"_l"+str(LAM)
 
-
+# Function to save configurations
 def save_config(phi, path):
     config = phi.detach().numpy().reshape(-1)
     np.savetxt(path, config)
 
 
+# Perform HMC
 for i in (range(ntraj)):
     flow_hmc(phi, model, tau=tau, n_steps=n_steps, reversibility=False)
     mag_i = phi.mean(axis=(1,2,3)).item()
 
-    with open(mesdir+wdir_prefix+"_mag", "a") as file_object:
+    with open(mesdir+wdir_prefix+"_mag.txt", "a") as file_object:
         file_object.write(str(mag_i)+"\n")
 
     if i % nsave == 0:
