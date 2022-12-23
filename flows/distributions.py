835bda2a38efdeaef23134eb63856e7995c9f9ae
dalbandea <albandeadavid@gmail.com>
2022-12-23 14:51:43 +0100

    Add Phi4 upscaled interpolated distribution

--- flows/distributions.py
+++ flows/distributions.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import torch
 
 from flows.utils import laplacian_2d
-from flows.phi_four import PhiFourActionBeta
+from flows.phi_four import PhiFourActionBeta, PhiUpInterpFourActionBeta
 
 Tensor: TypeAlias = torch.Tensor
 IterableDataset: TypeAlias = torch.utils.data.IterableDataset
@@ -69,7 +69,7 @@ class FreeScalarDistribution(torch.distributions.MultivariateNormal):
 
 
 class Phi4Dist(torch.distributions.Distribution):
-    def __init__(self, beta, lam, lsize, *, thermalization, discard):
+    def __init__(self, beta, lam, lsize, *, thermalization, discard, upscaling = 0):
         self.beta, self.lam, self.lsize = beta, lam, lsize
         self.phi = torch.zeros([lsize, lsize])
         self.phi.requires_grad = True
@@ -78,7 +78,10 @@ class Phi4Dist(torch.distributions.Distribution):
         self.thermalized = False
         self.action = lambda state: PhiFourActionBeta(self.beta,
                 self.lam)(state.view([-1, 1, self.lsize, self.lsize])) # hacky, PhiFourActionBeta needs the fields to have that shape...
+        self.upaction = lambda state: PhiFourActionBeta(self.beta,
+                self.lam)(state.view([-1, 1, 2*self.lsize, 2*self.lsize])) # upscaled action
         super(Phi4Dist, self).__init__(torch.Size(), validate_args=False)
+        self.upscaling = upscaling
         
     def sample(self, sample_shape=torch.Size()):
         samples = torch.zeros(sample_shape + [self.lsize, self.lsize])
@@ -94,10 +97,23 @@ class Phi4Dist(torch.distributions.Distribution):
             self.phi.requires_grad = False
             samples[i,:,:] = self.phi[:]
     
-        return samples
+        if self.upscaling:
+            upsamples = torch.zeros(sample_shape + [2*self.lsize,2*self.lsize])
+            upsamples[..., ::2, ::2] = samples[...,:,:]
+            upsamples[..., ::2, 1::2] = 1.0/2.0 * (samples + samples.roll(-1,-1))
+            upsamples[..., 1::2, ::2] = 1.0/2.0 * (samples + samples.roll(-1,-2))
+            upsamples[..., 1::2, 1::2] = 1.0/4.0 * (samples + samples.roll(-1,-2) +
+                    samples.roll(-1,-1) + samples.roll((-1,-1),(-2,-1)) )
+            return upsamples
+        else:
+            return samples
         
     def log_prob(self, phis: Tensor) -> Tensor:
-        return self.action(phis).neg().view([-1, 1, 1]) # hacky, to get Prior to compute the log_prob correctly...
+        if self.upscaling:
+            # return self.upaction(phis).neg().view([-1, 1, 1])
+            return 4*self.action(phis[..., ::2, ::2]).neg().view([-1, 1, 1])
+        else:
+            return self.action(phis).neg().view([-1, 1, 1]) # hacky, to get Prior to compute the log_prob correctly...
     
     def hmc(self, *, tau: float, n_steps: int) -> bool:
         phi_cp = torch.clone(self.phi).detach()
@@ -214,3 +230,189 @@ class Phi4Dist(torch.distributions.Distribution):
         #state.requires_grad_(False)
         #state.grad = None
         #return force
+
+
+class Phi4DistUpscaledInterpolated(torch.distributions.Distribution):
+    def __init__(self, beta, lam, lsize, *, thermalization, discard, upscaling = 0):
+        self.beta, self.lam, self.lsize = beta, lam, lsize
+        self.phi = torch.zeros([lsize, lsize])
+        self.phi.requires_grad = True
+        self.phi.grad = torch.zeros(self.phi.shape)
+        self.action = lambda state: PhiUpInterpFourActionBeta(self.beta,
+                self.lam)(state.view([-1, 1, self.lsize, self.lsize])) # hacky, PhiFourActionBeta needs the fields to have that shape...
+        self.thermalization, self.discard = thermalization, discard
+        self.thermalized = False
+        super(Phi4DistUpscaledInterpolated, self).__init__(torch.Size(), validate_args=False)
+        
+    def sample(self, sample_shape=torch.Size()):
+        samples = torch.zeros(sample_shape + [self.lsize, self.lsize])
+        
+        if not self.thermalized:
+            for i in range(self.thermalization):
+                self.hmc(tau = 1.0, n_steps = 10)
+            self.thermalized = True
+            
+        for i in range(sample_shape[0]):
+            for j in range(self.discard):
+                self.hmc(tau = 1.0, n_steps = 10)
+            self.phi.requires_grad = False
+            samples[i,:,:] = self.phi[:]
+    
+        upsamples = self.upscale_interp(samples)
+        return upsamples
+
+    def upscale_interp(self, samples):
+        sample_size = samples.size()
+        upsamples = torch.zeros([*sample_size[:-2]] + [2*self.lsize, 2*self.lsize])
+        upsamples[..., ::2, ::2] = samples[...,:,:]
+        upsamples[..., ::2, 1::2] = 1.0/2.0 * (samples + samples.roll(-1,-1))
+        upsamples[..., 1::2, ::2] = 1.0/2.0 * (samples + samples.roll(-1,-2))
+        upsamples[..., 1::2, 1::2] = 1.0/4.0 * (samples + samples.roll(-1,-2) +
+                samples.roll(-1,-1) + samples.roll((-1,-1),(-2,-1)) )
+        return upsamples
+
+    def upaction(self, phis: Tensor) -> Tensor:
+        return PhiFourActionBeta(self.beta, self.lam)(self.upscale_interp(phis).view([-1, 1, 2*self.lsize, 2*self.lsize]))
+
+    def log_prob(self, phis: Tensor) -> Tensor:
+        return PhiFourActionBeta(self.beta, self.lam)(phis.view([-1, 1,
+            2*self.lsize, 2*self.lsize])).neg().view([-1, 1, 1]) # this acts on the (up)samples
+    
+    def hmc(self, *, tau: float, n_steps: int) -> bool:
+        phi_cp = torch.clone(self.phi).detach()
+
+        self.phi.requires_grad = True
+        self.phi.grad = torch.zeros(self.phi.shape) # initialize gradient
+
+        # Initialize momenta
+        mom = torch.randn(self.phi.shape)
+
+        # Initial Hamiltonian
+        H_0 = self.hamiltonian(mom)
+
+        # Leapfrog integrator
+        self.leapfrog(mom, tau = tau, n_steps = n_steps)
+
+        # Final Hamiltonian
+        dH = self.hamiltonian(mom) - H_0
+
+        if dH > 0:
+            if torch.rand(1).item() >= torch.exp(-torch.Tensor([dH])).item():
+                with torch.no_grad():
+                    self.phi[:] = phi_cp # element-wise assignment
+                return False
+
+        return True
+    
+    def hamiltonian(self, mom):
+        """
+        Computes the Hamiltonian of `hmc` function.
+        """
+        H = 0.5 * torch.sum(mom**2) + self.action(self.phi)
+
+        return H.item()
+
+    def leapfrog_AD(self, mom, *, tau, n_steps):
+        dt = tau / n_steps
+
+        self.load_action_gradient()
+        mom -= 0.5 * dt * self.phi.grad
+
+        for i in range(n_steps):
+            with torch.no_grad():
+                self.phi += dt * mom
+
+            if i == n_steps-1:
+                self.load_action_gradient()
+                mom -= 0.5 * dt * self.phi.grad
+            else:
+                self.load_action_gradient()
+                mom -= dt * self.phi.grad
+                
+
+    def leapfrog(self, mom, *, tau, n_steps):
+        dt = tau / n_steps
+
+        mom -= 0.5 * dt * self.get_force(self.phi)
+
+        for i in range(n_steps):
+            with torch.no_grad():
+                self.phi += dt * mom
+
+            if i == n_steps-1:
+                mom -= 0.5 * dt * self.get_force(self.phi)
+            else:
+                mom -= dt * self.get_force(self.phi)
+
+    def get_force(self, state):
+        plaquettes = PhiUpInterpFourActionBeta(self.beta, self.lam).plaquette(state)
+        
+        frc = self.beta *   (
+                4 * state +
+                torch.roll(state, -1, 0) +
+                torch.roll(state, 1, 0) + torch.roll(state, -1, 1) +
+                torch.roll(state, 1, 1) 
+                + 1/2 * (plaquettes+plaquettes.roll(1,0)+plaquettes.roll(1,1)+plaquettes.roll((1,1),(0,1)))
+                ) - (1 - 2*self.lam) * (
+                        2 * state + 1/2 * (4 * state + torch.roll(state, -1, 0)
+                            + torch.roll(state, 1, 0) + torch.roll(state, -1, 1)
+                            + torch.roll(state, 1, 1))
+                        + 1/8
+                        *(plaquettes+plaquettes.roll(1,0)+plaquettes.roll(1,1)+plaquettes.roll((1,1),(0,1)))) - self.lam * (
+                                4 * state.pow(3) + 1/4 *
+                                ((state + state.roll(-1,0)).pow(3)+(state +
+                                    state.roll(1,0)).pow(3)+(state +
+                                        state.roll(-1,1)).pow(3)+(state +
+                                        state.roll(1,1)).pow(3))
+                        + 1/64
+                        *
+                        (plaquettes.pow(3)+plaquettes.roll(1,0).pow(3)+plaquettes.roll(1,1).pow(3)+plaquettes.roll((1,1),(0,1)).pow(3))
+                        ) 
+        
+
+        return -frc
+    
+    def check_force(self, epsilon):
+        
+        phi_check = torch.normal(0, 1, size=(self.lsize, self.lsize))
+        
+        phi_check_2 = torch.clone(phi_check)
+        phi_check_2[2,2] += epsilon
+        
+        F_num = (self.action(phi_check_2) - self.action(phi_check))/epsilon
+        F_ana = self.get_force(phi_check)[2,2]
+        
+        self.phi = phi_check
+        self.phi.requires_grad = True
+        self.phi.grad = torch.zeros(self.phi.shape)
+        self.load_action_gradient()
+        F_AD = self.phi.grad[2,2]
+        
+        print(F_num)
+        print(F_ana)
+        print(F_AD)
+
+    def check_action(self):
+        
+        phi_check = torch.normal(0, 1, size=(self.lsize, self.lsize))
+        
+        action1 = self.action(phi_check)
+        action2 = self.upaction(phi_check)
+
+        print(action1)
+        print(action2)
+
+    # Does not seem to work to train the network...
+    def load_action_gradient(self):
+        """
+        Passes `phi` through fucntion `action`and loads the gradient with respect to
+        the initial fields into `phi.grad`, without overwriting `phi`.
+        """
+        self.phi.grad.zero_()
+        
+        S = self.action(self.phi)
+        print(S.requires_grad)
+
+        external_grad_S = torch.ones(S.shape)
+
+        S.backward(gradient=external_grad_S)
--- flows/phi_four.py
+++ flows/phi_four.py
@@ -107,3 +107,67 @@ class PhiFourActionBeta:
 
     def log_prob(self, phi: Tensor) -> Tensor:
         return self.action(phi).neg()
+
+
+class PhiUpInterpFourActionBeta:
+    """Action for a scalar field on a lattice with quartic interaction with
+    interpolated variables.
+
+    Parameters
+    ----------
+    beta: float
+        Next-neighbour interaction
+    lam: float
+        Coupling for quartic interaction
+    """
+
+    def __init__(self, beta: float, lam: float):
+        self.beta = beta
+        self.lam = lam
+
+    def __call__(self, phi: Tensor) -> Tensor:
+        """Computes action for a sample of field configurations.
+
+        Parameters
+        ----------
+        phi: Tensor
+            The sample of configurations, where the first dimension
+            (``phi[i]``) runs over the configurations.
+
+        Returns
+        -------
+        Tensor
+            1-dimensional tensor containing the action for each
+            configuration in the sample.
+        """
+        action = torch.zeros_like(phi)
+
+        # # Interaction with nearest neighbours
+        for dim in range(2, phi.dim()):
+            action.sub_(
+                    phi.add(phi.roll(-1, dim)).pow(2).mul(0.5).mul(self.beta)
+                        )
+        action.sub_(
+                self.plaquette(phi).pow(2).mul(self.beta).mul(0.25)
+                )
+
+        # # phi^2 term
+        action.add_(phi.pow(2).add((2*phi+phi.roll(-1,-1)+phi.roll(-1,-2)).mul(0.25)).mul(1-2*self.lam)
+                + 1/16 * self.plaquette(phi).pow(2) * (1-2*self.lam))
+
+        # # phi^4 term
+        action.add_(self.lam * (phi.pow(4) + 1/16 * ((phi +
+            phi.roll(-1,-1)).pow(4) + (phi + phi.roll(-1,-2)).pow(4)) 
+            + 1/256 * self.plaquette(phi).pow(4))
+            )
+
+        # Sum over lattice sites
+        action = action.flatten(start_dim=1).sum(dim=1)
+
+        return action
+
+    def plaquette(self, phi):
+        return phi.add(phi.roll(-1,-1)).add(phi.roll(-1,-2)).add(phi.roll((-1,-1),(-2,-1)))
+
+    def log_prob(self, phi: Tensor) -> Tensor:
+        return self.action(phi).neg()
